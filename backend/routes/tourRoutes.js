8136b0de454463c04084055b24793420c42abedf
ThulaniDissanayake <IT22157232@my.sliit.lk>
2025-08-26 10:42:14 +0530

    completed tour routes

--- backend/routes/tourRoutes.js
+++ backend/routes/tourRoutes.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import { getAllTours, getTourById, createTour, deleteTour, updateTour } from '../controllers/tourController.js';
-
 import authMiddleware from '../middleware/authMiddleware.js';
 import adminMiddleware from '../middleware/adminMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', getAllTours);
-router.get('/:id', getTourById);
-router.post('/', authMiddleware, adminMiddleware, createTour);
-router.put('/:id', authMiddleware, adminMiddleware, updateTour);
-router.delete('/:id', authMiddleware, adminMiddleware, deleteTour);
+router.get('/', getAllTours);           
+router.get('/:id', getTourById);        
+router.post('/', authMiddleware, adminMiddleware, createTour);  
+router.put('/:id', authMiddleware, adminMiddleware, updateTour); 
+router.delete('/:id', authMiddleware, adminMiddleware, deleteTour); 
 
 export default router;
