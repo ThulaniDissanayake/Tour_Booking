c8a4dd6fec509216a513fe1807caf768eecd18ef
ThulaniDissanayake <IT22157232@my.sliit.lk>
2025-08-26 10:48:40 +0530

    updated user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,8 @@ import { registerUser, loginUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
-// Public route - user registration
-router.post('/register', registerUser);
 
-// Public route - user login
+router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 export default router;
